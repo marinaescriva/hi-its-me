9d0d9d88a1d483adc6e89c327fc78ac3269a29c8
marinaescriva <marinaescriva.24@gamil.com>
2024-03-11 20:58:47 +0100

    feature: seeder of posts created

--- src/database/seeders/postSeeder.js
+++ src/database/seeders/postSeeder.js
@@ -2,23 +2,24 @@ import Post from "../../models/Post.js";
 import { fakerES as faker } from '@faker-js/faker';
 import User from "../../models/User.js";
 
-const users = User.find({});
+
 
 
 const seedPosts = async () => {
     const posts = [];
+    const users = await User.find({});
 
     for (let i = 0; i < 10; i++) {
 
+        const randomUser = Math.floor(Math.random() * users.length);
         const post = {
 
-            title: faker..firstName(),
-            email: faker.internet.email(),
-            password: "123456",
-            role: "user"
-
+            title: faker.lorem.lines({min:1, max:3}),
+            text: faker.lorem.text(),
+            nick: users[randomUser]._id
+    
         }
-        users.push(user);
+        posts.push(post);
 
     }
     await Post.create(posts);
--- src/database/seeders/seed.js
+++ src/database/seeders/seed.js
@@ -1,18 +1,21 @@
 import { dbConnection } from "../db.js";
 import mongoose from "mongoose";
-import userSeeder from "./userSeeder.js";
+// import userSeeder from "./userSeeder.js";
 import "dotenv/config.js";
+import seedPosts from "./postSeeder.js";
 
 const seed = async () => {
     try {
         await dbConnection();
         console.log("database is connected")
-        await userSeeder();
+        // await userSeeder();
+        await seedPosts();
 
 
     } catch (error) {
         console.log("database connection is failed");
         console.error(error.message);
+
     } finally {
         await mongoose.connection.close();
         console.log("database connection closed");
